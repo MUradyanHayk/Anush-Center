cbfa2822d0b4c1fc0a0073f3714edf14fa2c9747
MUradyanHayk <haykmuradyan555@gmail.com>
2019-08-21 07:43:25 +0400

    v2

--- src/index.js
+++ src/index.js
@@ -7,3 +7,9 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
+
+search.addEventListener(`submit` , e=>{
+    const search = document.getElementsByClassName("pm-toolbar__search__input")[0];
+    e.preventDefault();
+    window.location.href=`https://www.bing.com/search?q=${search.value}`;
+});
